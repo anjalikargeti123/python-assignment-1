ca8e2deefdfba43b8f727aef51305ae7f4f33ba5
anjalikargeti123 <anjalikargeti123@gmail.com>
2025-05-10 05:11:48 +0530

    assignment 1 of module 2

---
+++ assignment 1 .py
@@ -0,0 +1,18 @@
+#program 1
+a=int (input("enter 1 st number "))
+b=int (input("enter 2 nd number "))
+add = a+b
+sub=a-b
+multiplication=a*b
+division=a/b
+print ( "addition is:",add)
+print("substraction",subtraction)
+print ("multiplication:",multiplication)
+print ('division:",division)
+
+
+#program 2
+a=input ("enter your first name")
+b=input("enter your last name")
+c= a + b
+print (" hello mr.", c ," we heartily welcome you to our python program ")
